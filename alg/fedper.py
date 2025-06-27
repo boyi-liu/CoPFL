03b6aa6258f8f9b6595dac8cd6d9354bdb1032e2
Boyi Liu <byliu0325@gmail.com>
2025-06-27 13:52:12 +0800

    add fedper.py

---
+++ alg/fedper.py
@@ -0,0 +1,17 @@
+from alg.base import BaseClient, BaseServer
+from utils.time_utils import time_record
+
+p_keys = ['fc']
+
+class Client(BaseClient):
+    @time_record
+    def run(self):
+        self.train()
+
+class Server(BaseServer):
+    def run(self):
+        self.sample()
+        self.downlink()
+        self.client_update()
+        self.uplink()
+        self.aggregate()
