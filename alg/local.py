0872cdfaa948352747b0ebc50c9e38145c944b39
Boyi Liu <byliu0325@gmail.com>
2025-06-27 13:34:45 +0800

    add local.py

---
+++ alg/local.py
@@ -0,0 +1,18 @@
+from alg.base import BaseClient, BaseServer
+from utils.time_utils import time_record
+
+class Client(BaseClient):
+    @time_record
+    def run(self):
+        self.train()
+
+    def clone_model(self, target):
+        pass
+
+class Server(BaseServer):
+    def run(self):
+        self.sample()
+        # self.downlink()
+        self.client_update()
+        # self.uplink()
+        # self.aggregate()
